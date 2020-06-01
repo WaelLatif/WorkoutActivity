1962c7611e6c5b43d7d4ebb5102d832668addddf
wael devo <65243494+waelDevo@users.noreply.github.com>
2020-06-01 16:13:28 +0200

    WorkoutActiviyApp

---
+++ app/src/main/java/com/example/workoutactivity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.workoutactivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class MainActivity extends Activity {
+    private WorkoutExport export = new WorkoutExport();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickfindworkout (View view)
+    {
+        TextView workouts=  (TextView) findViewById(R.id.workout);
+        Spinner workouttype = (Spinner) findViewById(R.id.Workouttype);
+        String workout = String.valueOf(workouttype.getSelectedItem());
+
+        //workouts.setText(workout)
+        List <String> workoutlist = export.getWorkouts(workout);
+        StringBuilder workoutsformatted= new StringBuilder();
+        for (String work: workoutlist){
+            workoutsformatted.append(work).append("\n");
+        }
+        workouts.setText(workoutsformatted);
+
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/workoutactivity/WorkoutExport.java
@@ -0,0 +1,49 @@
+package com.example.workoutactivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WorkoutExport {
+
+    List<String> getWorkouts (String workouttypes)
+    {
+        List<String> workout = new ArrayList<String>();
+        if (workouttypes.equals("Chest")) {
+            workout.add("Dumbbell Squeeze Press");
+            workout.add("Incline barbell bench press");
+            workout.add("Close-grip barbell bench press");
+            workout.add("Decline press-up");
+            workout.add("Cable fly");
+            workout.add("Staggered press-up");
+            workout.add("Chest dips");
+        } else if (workouttypes.equals("Biceps")) {
+            workout.add("Fat-Grip Hammer Curl");
+            workout.add("Behind-the-Back Cable Curl");
+            workout.add("EZ-Bar Preacher Curl");
+            workout.add("Reverse Curl");
+            workout.add("Wide-Grip Curl");
+            workout.add("Close-Grip Curl");
+        } else if (workouttypes.equals("Triceps")) {
+            workout.add("Close-grip Bench Press");
+            workout.add("Rope Tricep Pushdown");
+            workout.add("Tricep Dips Advanced");
+            workout.add("Overhead Triceps Extension");
+            workout.add("Skullcrushers");
+            workout.add("The Diamond Press-up");
+        } else if (workouttypes.equals("Shoulders")) {
+            workout.add("Barbell Overhead Press");
+            workout.add("Standing Dumbbell Fly");
+            workout.add("Face Pull");
+            workout.add("High Pull");
+            workout.add("Seated Dumbbell Clean");
+            workout.add("Trap Raise");
+        }
+        return workout ;
+
+    }
+
+
+
+
+
+}
